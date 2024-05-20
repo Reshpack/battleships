c3e06fa69926facadd6924c10f31cbb7c25952b9
Suresh Packiry <sureshpackiry73@gmail.com>
2024-05-20 19:46:46 +1000

    still figuring out formats

--- src/index.js
+++ src/index.js
@@ -1,8 +1,2 @@
 import './styles/main.scss'
-import './styles/title.css'
 
-import backgroundImage from './assets/stars.png'; // Adjust the path to your image
-
-document.addEventListener('DOMContentLoaded', () => {
-  document.body.style.backgroundImage = `url(${backgroundImage})`;
-});
