6be3995dc5bc9f109217f2ce9fe034d379fa2d4e
Suresh Packiry <sureshpackiry73@gmail.com>
2024-05-23 12:22:11 +1000

    redo

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import './styles/main.scss'
 
+const optionCont = document.querySelector('#option-container');
+const flipBtn = document.querySelector('#flip-button');
+
+
+function flip() {
+    
+};
+
+flipBtn.addEventListener('click', flip)
